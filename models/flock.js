b5f41fbb077129f263f32bd84223f52649772f1e
Martin van Harmelen <Martin@vanharmelen.com>
2016-11-13 01:47:15 +0100

    Removed (hopefully) redundant client configuration from models/flock.js

--- models/flock.js
+++ models/flock.js
@@ -6,7 +6,7 @@ var ardrone = require('ar-drone');
 
 // Max vertical speed in MM/s
 var CONTROL_Z_SPEED = 2000;
-var CONTROL_MAX_HEIGHT = 5000;
+var CONTROL_MAX_HEIGHT = 3000;
 var CONTROL_MIN_HEIGHT = 50;
 
 
@@ -27,20 +27,20 @@ var Drone = function(_id, base_ip) {
   //  - T0 is the last point
   //  - T1 is the point before that one
   //  - TX contains a array with all points
-  this.location = { 
+  this.location = {
     t0: { x: 0, y: 0, z: 0, r: 0 },
     t1: { x: 0, y: 0, z: 0, r: 0 },
     tX: []
   };
 
-  // The navdata 
+  // The navdata
   this.navdata = {
     t0: { timestamp: new Date().getTime() },
     t1: { timestamp: new Date().getTime() },
     tX: []
   };
 
-  // The calculated 
+  // The calculated
   this.go = {
     control: { vx: 0, vy: 0, vz: 0, vYaw: 0 },
     autopilot: { vx: 0, vy: 0, vz: 0, vYaw: 0 },
@@ -55,24 +55,19 @@ var Drone = function(_id, base_ip) {
   this.client.config('control:flying_mode', 0); // Default to NOT autopilot mode
   //this.client.config('pic:ultrasound_freq', 7 + (stageListDrones.indexOf(drone._id) % 2))
   this.client.config('general:navdata_demo', 'TRUE');
-  this.client.config('general:navdata_options', 65536 + 8388608 + 67108864); 
-
-  this.client.config('control:control_vz_max', CONTROL_Z_SPEED);
-
-  //drone.this.client.config('control:altitude_min',   50)
-  //drone.this.client.config('control:altitude_max', 3000)
-
   // 1024 -> Altitude
   // 65536 -> Detect
   // 8388608 -> Wind speed
   // 67108864 -> WiFi
+  // this.client.config('general:navdata_options', 65536 + 8388608 + 67108864);
+  // this.client.config('video:codec_fps', 1);
 
-  this.client.config('video:codec_fps', 1);
-  this.client.config('control:altitude_max','3000'); // 3 meter
-  this.client.config('control:altitude_min','50'); // 5 cm
+  this.client.config('control:control_vz_max', CONTROL_Z_SPEED);
+  this.client.config('control:altitude_max', CONTROL_MAX_HEIGHT);
+  this.client.config('control:altitude_min', CONTROL_MIN_HEIGHT);
   // this.client.config('detect:detect_type', 12);  // detect roundell
 
-  this.client.animateLeds('red', 1,1);
+  // this.client.animateLeds('red', 1,1);
 
   /* Signal landed and flying events.
   this.client.on('landing', function () { });
@@ -80,8 +75,8 @@ var Drone = function(_id, base_ip) {
   this.client.on('takeoff', function() { });
   this.client.on('hovering', function() { });
   this.client.on('flying', function() { });*/
-  
-  // Save every navdata
+
+  // Save incoming navdata (keeps two most recent)
   var navdata = this.navdata;
   this.client.on('navdata', function(data) {
     navdata.t1 = navdata.t0;
